35cfebbf20e2e4eabebaec263a69f8c6f56eb338
vina.kumar <vina.kumar@servicenow.com>
2018-11-18 20:55:53 +0530

    my-rotuing-start: fecting route params

--- my-routing-start/src/app/users/user/user.component.ts
+++ my-routing-start/src/app/users/user/user.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Params } from '@angular/router/src/shared';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-  }
+    this.user={
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+
+    this.paramsSubscription =  this.route.params.subscribe(
+      (params: Params)=> {
+        this.user.id=params['id'];
+        this.user.name= params['name'];
+      }
+    );
+  } 
+    ngOnDestroy() {
+      this.paramsSubscription.unsubscribe();
+    }
+
+  
 
 }
