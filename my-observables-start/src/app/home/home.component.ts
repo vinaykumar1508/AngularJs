931d1f554f045b79c3bba07493e5dc0d44696b9e
vina.kumar <vina.kumar@servicenow.com>
2018-11-27 07:06:45 +0530

    added observables and unsubscribe

--- my-observables-start/src/app/home/home.component.ts
+++ my-observables-start/src/app/home/home.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
+import { Observer } from 'rxjs';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit, OnDestroy {
+  
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
   constructor() { }
 
   ngOnInit() {
+    
+    const myNumbers = Observable.interval(1000);
+    this.numbersObsSubscription= myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    );
+    
+
+    const myObservable = Observable.create((observer: Observer<String>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+       // observer.error('this does not work');
+       observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        observer.next('third package');
+      }, 6000);
+    });
+
+    this.customObsSubscription = myObservable.subscribe(
+      (data: string) => { console.log(data);},
+      (error: string) => { console.log(error);},
+      () => {console.log('completed');}
+    );
+
+  }
+
+  ngOnDestroy(){
+    this.customObsSubscription.unsubscribe();
+    this.numbersObsSubscription.unsubscribe();
   }
 
 }
