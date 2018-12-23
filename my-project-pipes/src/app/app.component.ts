4da0acd68700496349fd7ac730079744b04814cd
vina.kumar <vina.kumar@servicenow.com>
2018-12-23 12:07:55 +0530

    pipe tutorial completed

--- my-project-pipes/src/app/app.component.ts
+++ my-project-pipes/src/app/app.component.ts
@@ -6,10 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  appStatus = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+      resolve('stable');
+    },2000);
+  });
+
   servers = [
     {
       instanceType: 'medium',
-      name: 'Production Server',
+      name: 'Production',
       status: 'stable',
       started: new Date(15, 1, 2017)
     },
@@ -39,4 +46,15 @@ export class AppComponent {
       'list-group-item-danger': server.status === 'critical'
     };
   }
+
+  onAddServer(){
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    });
+  }
+
+
 }
